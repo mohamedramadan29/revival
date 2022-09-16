4f54aa4045203a8d040815e9073b4ce92c203a77
mohamed <mr319242@gmail.com>
2022-09-16 17:29:22 +0200

    new

--- admin_event/event_add_section/delete.php
+++ admin_event/event_add_section/delete.php
@@ -1,17 +1,17 @@
 <?php
-if (isset($_GET['speaker_id']) && is_numeric($_GET['speaker_id'])) {
-    $speaker_id = $_GET['speaker_id'];
+if (isset($_GET['add_id']) && is_numeric($_GET['add_id'])) {
+    $add_id = $_GET['add_id'];
 
-    $stmt = $connect->prepare('SELECT * FROM  event_speaker WHERE speaker_id= ?');
-    $stmt->execute([$speaker_id]);
+    $stmt = $connect->prepare('SELECT * FROM addition_section WHERE add_id= ?');
+    $stmt->execute([$add_id]);
     $count = $stmt->rowCount();
     if ($count > 0) {
-        $stmt = $connect->prepare('DELETE FROM  event_speaker WHERE speaker_id=?');
-        $stmt->execute([$speaker_id]);
+        $stmt = $connect->prepare('DELETE FROM  addition_section WHERE add_id=?');
+        $stmt->execute([$add_id]);
         if ($stmt) { ?>
 <div class="alert-success">
     <?php echo $lang['delete_message']; ?>
-    <?php header('LOCATION:main.php?dir=event_speakers&page=report'); ?>
+    <?php header('LOCATION:main.php?dir=event_add_section&page=report'); ?>
     <?php }
     }
 }
